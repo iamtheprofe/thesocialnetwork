4b3c4b1e1e4cf6b582b89aa585e9f8bc69fa6767
Daniel Garcia Alvarado <danigarciaalva@hotmail.com>
2023-03-13 00:05:45 -0600

    Added launcher to different activities

--- app/src/main/java/com/example/thesocialnetwork/forgotPassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/thesocialnetwork/forgotPassword/ForgotPasswordActivity.kt
@@ -1,13 +1,13 @@
-package com.example.thesocialnetwork.signup
+package com.example.thesocialnetwork.forgotPassword
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thesocialnetwork.R
 
-class SignUpActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_forgot_password)
     }
 }
