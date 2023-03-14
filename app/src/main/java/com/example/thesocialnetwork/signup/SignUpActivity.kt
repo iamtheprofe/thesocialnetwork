30845db4adb7638e08694efed799c26afc58fb14
juanbielma <juan.bielma@globant.com>
2023-03-14 01:23:11 -0600

    Updated SpannableString on Terms and conditions text view, removed new typography styles

--- app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
@@ -1,12 +1,17 @@
 package com.example.thesocialnetwork.signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.SpannableString
+import android.text.TextPaint
 import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.text.style.TextAppearanceSpan
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thesocialnetwork.R
+import com.example.thesocialnetwork.login.LoginActivity
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -15,16 +20,24 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        findViewById<TextView>(R.id.login).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
         configTermsAndPrivacySpan()
     }
 
     private fun configTermsAndPrivacySpan() {
         val termsConditionsPrivacyTextView = findViewById<TextView>(R.id.termsConditionsPrivacy)
+
         val termsConditionsPrivacyText = getString(R.string.feat_sign_up_terms_privacy)
         val termsConditionsText = getString(R.string.feat_sign_up_terms_and_conditions)
         val privacyText = getString(R.string.feat_sign_up_privacy_policy)
-        val termsLinkTextStyle = TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link_Caption)
-        val privacyLinkTextStyle = TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link_Caption)
+
+        val termsLinkTextStyle =
+            TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link)
+        val privacyLinkTextStyle =
+            TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link)
 
         val spannableString = SpannableString(termsConditionsPrivacyText)
 
@@ -32,6 +45,23 @@ class SignUpActivity : AppCompatActivity() {
         val endIndexTermsConditions = startIndexTermsConditions + termsConditionsText.length
         spannableString.setSpan(
             termsLinkTextStyle,
+            startIndexTermsConditions,
+            endIndexTermsConditions,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val termsClickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                println("termsClickableSpan")
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+        spannableString.setSpan(
+            termsClickableSpan,
             startIndexTermsConditions,
             endIndexTermsConditions,
             SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
@@ -45,8 +75,22 @@ class SignUpActivity : AppCompatActivity() {
             endIndexPrivacy,
             SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
         )
+        val privacyClickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                println("privacyClickableSpan")
+            }
 
-
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+        spannableString.setSpan(
+            privacyClickableSpan,
+            startIndexPrivacy,
+            endIndexPrivacy,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
 
         termsConditionsPrivacyTextView.text = spannableString
         termsConditionsPrivacyTextView.movementMethod = LinkMovementMethod.getInstance()
