94debc26643bfc5c8e7948611b8b29d1e15b2440
juanbielma <juan.bielma@globant.com>
2023-03-13 22:11:05 -0600

    Added Sign Up activity design

--- app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
@@ -1,13 +1,54 @@
 package com.example.thesocialnetwork.signup
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.TextAppearanceSpan
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thesocialnetwork.R
+
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        configTermsAndPrivacySpan()
+    }
+
+    private fun configTermsAndPrivacySpan() {
+        val termsConditionsPrivacyTextView = findViewById<TextView>(R.id.termsConditionsPrivacy)
+        val termsConditionsPrivacyText = getString(R.string.feat_sign_up_terms_privacy)
+        val termsConditionsText = getString(R.string.feat_sign_up_terms_and_conditions)
+        val privacyText = getString(R.string.feat_sign_up_privacy_policy)
+        val termsLinkTextStyle = TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link_Caption)
+        val privacyLinkTextStyle = TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link_Caption)
+
+        val spannableString = SpannableString(termsConditionsPrivacyText)
+
+        val startIndexTermsConditions: Int = termsConditionsPrivacyText.indexOf(termsConditionsText)
+        val endIndexTermsConditions = startIndexTermsConditions + termsConditionsText.length
+        spannableString.setSpan(
+            termsLinkTextStyle,
+            startIndexTermsConditions,
+            endIndexTermsConditions,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val startIndexPrivacy: Int = termsConditionsPrivacyText.indexOf(privacyText)
+        val endIndexPrivacy = startIndexPrivacy + privacyText.length
+        spannableString.setSpan(
+            privacyLinkTextStyle,
+            startIndexPrivacy,
+            endIndexPrivacy,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+
+
+        termsConditionsPrivacyTextView.text = spannableString
+        termsConditionsPrivacyTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
