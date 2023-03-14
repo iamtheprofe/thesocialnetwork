e5d9425572f91c8e5c9ef22c67dcf957c42e9ac8
MurilloSama <yordy.murillo.clbdf@gmail.com>
2023-03-14 05:19:48 -0500

    sign_up_activity
    designed the xml activity_sign_up,
    
    coded  the SignUpActivity to have 2 links on a string using a Spannable string,
    
    added Strings to de stringsxml

--- app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
@@ -1,32 +1,21 @@
 package com.example.thesocialnetwork.signup
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.TextAppearanceSpan
-import android.util.Patterns
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thesocialnetwork.R
-import com.example.thesocialnetwork.login.LoginActivity
-import com.google.android.material.textfield.TextInputEditText
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-        findViewById<EditText>(R.id.login).setOnClickListener{
-            startActivity(Intent(this,LoginActivity::class.java))
-        }
 
         val textView = findViewById<TextView>(R.id.agreement)
         val agreement = textView.text.toString()
@@ -39,12 +28,12 @@ class SignUpActivity : AppCompatActivity() {
             override fun onClick(widget: View) {
             }
         }
-        val startIndex1 = agreement.indexOf(getString(R.string.feat_sign_up_terms_conditions))
-        val endIndex1 = startIndex1 + (getString(R.string.feat_sign_up_terms_conditions)).length
+        val startIndex1 = agreement.indexOf("Terms & Conditions")
+        val endIndex1 = startIndex1 + "Terms & Conditions".length
         builder.setSpan(termsConditions, startIndex1, endIndex1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
 
-        val startIndex2 = agreement.indexOf(getString(R.string.feat_signup_privacy_policy))
-        val endIndex2 = startIndex2 + (getString(R.string.feat_sign_up_privacy_policy)).length
+        val startIndex2 = agreement.indexOf("Privacy Policy")
+        val endIndex2 = startIndex2 + "Privacy Policy".length
         builder.setSpan(privacyPolicy, startIndex2, endIndex2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         val linkStyle = TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link)
@@ -53,43 +42,5 @@ class SignUpActivity : AppCompatActivity() {
 
         textView.setText(builder, TextView.BufferType.SPANNABLE)
         textView.movementMethod = LinkMovementMethod.getInstance()
-
-        val continueButton = findViewById<Button>(R.id.continue_button)
-        continueButton.setOnClickListener {
-            handleContinue()
-        }
-    }
-
-    private fun handleContinue() {
-
-        val email = findViewById<TextInputEditText>(R.id.emailInputLayout)
-        val fullName = findViewById<TextInputEditText>(R.id.fullName)
-        val mobile = findViewById<TextInputEditText>(R.id.mobile)
-
-        val isValidEmail = isValidEmail(email.text.toString())
-        val isValidName = isValidName(fullName.text.toString())
-        val isValidMobile = isValidMobile(mobile.text.toString())
-
-        if (isValidEmail && isValidName && isValidMobile) {
-            Toast.makeText(this, "Credential are correct", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Credentials are incorrect", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun isValidEmail(email: String): Boolean {
-        val cleanEmail = email.trim().lowercase()
-        return Patterns.EMAIL_ADDRESS.matcher(cleanEmail)
-            .matches() && cleanEmail == "murillosamastream@gmail.com"
-    }
-
-    private fun isValidName(fullName: String): Boolean {
-        val cleanName = fullName.trim()
-        return cleanName == "Yordy Murillo"
-    }
-
-    private fun isValidMobile(mobile: String): Boolean {
-        val cleanMobile = mobile.trim()
-        return cleanMobile == "123125123"
     }
 }
