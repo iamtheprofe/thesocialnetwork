8ac353d56e109de64c03f492db8a8970fa80c34c
MurilloSama <yordy.murillo.clbdf@gmail.com>
2023-03-14 05:19:48 -0500

    sign_up_activity
    designed the xml activity_sign_up,
    
    coded  the SignUpActivity to have 2 links on a string using a Spannable string,
    
    added Strings to de stringsxml

--- app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
@@ -1,6 +1,13 @@
 package com.example.thesocialnetwork.signup
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.TextAppearanceSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thesocialnetwork.R
 
@@ -9,5 +16,31 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val textView = findViewById<TextView>(R.id.agreement)
+        val agreement = textView.text.toString()
+        val builder = SpannableStringBuilder(agreement)
+        val termsConditions = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+            }
+        }
+        val privacyPolicy = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+            }
+        }
+        val startIndex1 = agreement.indexOf("Terms & Conditions")
+        val endIndex1 = startIndex1 + "Terms & Conditions".length
+        builder.setSpan(termsConditions, startIndex1, endIndex1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val startIndex2 = agreement.indexOf("Privacy Policy")
+        val endIndex2 = startIndex2 + "Privacy Policy".length
+        builder.setSpan(privacyPolicy, startIndex2, endIndex2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val linkStyle = TextAppearanceSpan(this, R.style.TheSocialNetwork_TextStyle_Link)
+        builder.setSpan(linkStyle, startIndex1, endIndex1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        builder.setSpan(linkStyle, startIndex2, endIndex2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textView.setText(builder, TextView.BufferType.SPANNABLE)
+        textView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
