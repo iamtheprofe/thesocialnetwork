cfd122eb9d917521d8597713cc6ecc7588070b89
diegomaxter <diegomaxter0@gmail.com>
2023-03-13 21:05:14 -0500

    diegomaxter--signup-mockup

--- app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/thesocialnetwork/signup/SignUpActivity.kt
@@ -9,5 +9,6 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
     }
 }
