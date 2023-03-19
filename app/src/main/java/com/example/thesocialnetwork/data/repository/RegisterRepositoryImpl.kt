d59c5f8cc06b22968a38bb7cb2a0c8dc0842e744
gianpaul <gianprp@hotmail.com>
2023-03-19 14:53:59 -0500

    Fix - fix the function senData with let

--- app/src/main/java/com/example/thesocialnetwork/data/repository/RegisterRepositoryImpl.kt
+++ app/src/main/java/com/example/thesocialnetwork/data/repository/RegisterRepositoryImpl.kt
@@ -2,24 +2,25 @@ package com.example.thesocialnetwork.data.repository
 
 import com.example.thesocialnetwork.data.api.RegisterApi
 import com.example.thesocialnetwork.data.api.dto.user.RegisterRequestDto
-import com.example.thesocialnetwork.data.api.dto.user.RegisterResponseDto
-import com.example.thesocialnetwork.models.Register
+import com.example.thesocialnetwork.model.Register
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RegisterRepositoryImpl(private val registerApi: RegisterApi):RegisterRepository {
+class RegisterRepositoryImpl(private val registerApi: RegisterApi) : RegisterRepository {
 
     private val retrofit = Retrofit.Builder()
-         .baseUrl("https://reqres.in/api")
-         .addConverterFactory(GsonConverterFactory.create())
-         .build()
+        .baseUrl("https://reqres.in/api")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
-   private val api = retrofit.create(RegisterApi::class.java)
+    private val api = retrofit.create(RegisterApi::class.java)
 
     override fun sendData(email: String, password: String): Register {
-    val response = api.sendData(RegisterRequestDto(email, password)).execute()
-        if(response.isSuccessful &&response.body()!=null) {
-            return Register(response.body().token)
+        val response = api.sendData(RegisterRequestDto(email, password)).execute()
+        if (response.isSuccessful) {
+            return Register(response.body()?.token ?: "")
+        } else {
+            throw Exception("Error")
         }
     }
 
