030456426db5c387fd51178f869dd1d390f6504a
Daniel Garcia Alvarado <danigarciaalva@hotmail.com>
2023-03-21 22:10:31 -0600

    Recycler Views course

---
+++ app/src/main/java/com/example/thesocialnetwork/feed/FeedActivity.kt
@@ -0,0 +1,40 @@
+package com.example.thesocialnetwork.feed
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.thesocialnetwork.Creator
+import com.example.thesocialnetwork.Post
+import com.example.thesocialnetwork.R
+import com.example.thesocialnetwork.User
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val posts = arrayListOf<Post>()
+        (0..500).forEach {
+            posts.add(
+                Post(
+                    id = it.toString(),
+                    text = "Muri se la come ${it + 1} veces",
+                    creator = Creator(
+                        user = User(
+                            fullName = "Daniel Garcia"
+                        )
+                    )
+                )
+            )
+        }
+        recyclerView.adapter = FeedAdapter(
+            posts = posts
+        )
+    }
+}
